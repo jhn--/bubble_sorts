5de777f536b12efb84a8028e9a5a7bbc436f25e2
John <jhn--@users.noreply.github.com>
2021-05-15 18:10:20 +0800

    Create bubble_for.py
    major change - 
    no more `while` loop, `for` loops used instead.
    
    still recursive in nature.

---
+++ bubble_for.py
@@ -0,0 +1,24 @@
+def bubble(L, swap=None):
+    """Helper function for bubble sort
+
+    Parameters:
+    L (list): Unsorted (eventually sorted) list.
+    swap (boolean): Whether list's elements have been swapped before, serves as an indicator on whether we're done sorting.
+
+    Returns:
+    L (list): Sorted list.
+    """
+    print(f'{L} : {swap}')
+    if len(L) == 0:
+        return L
+    if swap == True or swap == None:
+        swap = False
+        for i in range(len(L)):
+            if i == len(L)-1:
+                return bubble(L, swap)
+            ii = i + 1
+            if L[i] > L[ii]:
+                (L[i], L[ii]) = (L[ii], L[i])
+                swap = True
+    else:
+        return L
